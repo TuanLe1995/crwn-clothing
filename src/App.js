a1b516a4288891a869ee4e5dc7781ce0ead82051
TuanLe1995 <leanhtuan9142@gmail.com>
2020-04-12 19:35:39 -0500

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 // Switch: the moment it finds the matched path, its not gonna render any paths below the path.
 // Route receives three arguments : exact(boolean) path(string) component(JSX)
 // The component properties of Route is gonna get passed three arguments: history, location, and match
 function App() {
   return (
-    <div>
+    <div className="">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
